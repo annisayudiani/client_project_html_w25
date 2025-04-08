affb4de7d89d2fb9cd2e47180a963404a1dd268d
Annisa Permata Yudiani <yudiani.annisa@gmail.com>
2025-04-07 20:59:27 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Scroll line animation source code: https://codepen.io/bogdansandu/pen/NWQzyWa
+
+const scrollLine = document.querySelector('.scroll-line');
+
+function updateScrollProgress() {
+  const windowHeight = window.innerHeight;
+  const fullHeight = document.body.clientHeight;
+  const scrolled = window.scrollY;
+  const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
+  scrollLine.style.width = `${percentScrolled}%`;
+}
+
+updateScrollProgress();
+
+let ticking = false;
+window.addEventListener('scroll', () => {
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      updateScrollProgress();
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
+
+window.addEventListener('resize', updateScrollProgress);
